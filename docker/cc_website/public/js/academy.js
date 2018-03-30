1116af147435630afad7a841eced05a9f7f199fa
shahnirav <shah.nirav@me.com>
2018-03-31 00:43:27 +0100

    commit

--- docker/cc_website/public/js/academy.js
+++ docker/cc_website/public/js/academy.js
@@ -25,14 +25,9 @@ $(function () {
         }
     });
 
-// Create a Stripe client.
     var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
-
-// Create an instance of Elements.
     var elements = stripe.elements();
 
-// Custom styling can be passed to options when creating an Element.
-// (Note that this demo uses a wider set of styles than the guide below.)
     var style = {
         base: {
             color: '#32325d',
@@ -50,13 +45,10 @@ $(function () {
         }
     };
 
-// Create an instance of the card Element.
     var card = elements.create('card', {style: style});
-
-// Add an instance of the card Element into the `card-element` <div>.
     card.mount('#card-element');
 
-// Handle real-time validation errors from the card Element.
+
     card.addEventListener('change', function (event) {
         var displayError = document.getElementById('card-errors');
         if (event.error) {
