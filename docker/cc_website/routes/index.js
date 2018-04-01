43de98b1d301206b22650587b3b3a3080d209cff
shahnirav <shah.nirav@me.com>
2018-04-01 23:19:24 +0100

    commit

--- docker/cc_website/routes/index.js
+++ docker/cc_website/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+var stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Jain Academy Payments', payment_title: "Fundamentals of Jainism : August 2018"});
@@ -8,7 +8,24 @@ router.get('/', function (req, res, next) {
 
 router.post('/issueTicket', function (req, res, next) {
     console.log(req.body);
-    res.json({status: "ok"})
+    var token = req.body.stripeToken;
+    var paymentAmount = req.body.paymentAmount;
+    var email = req.body.inputEmail;
+    stripe.charges.create({
+        amount: paymentAmount,
+        currency: "gbp",
+        description: "Example charge",
+        source: token,
+    }, function (err, charge) {
+        if (err) {
+            console.log(err);
+            res.status(500).json({error: err})
+        } else {
+            console.log(charge);
+            res.json({status: "ok"})
+        }
+    });
+
 });
 
 module.exports = router;
