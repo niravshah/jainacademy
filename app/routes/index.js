82bd4c47eaeb0e6e6e9404acbbbf62fb4ce9efae
shahnirav <shah.nirav@me.com>
2018-04-02 13:10:56 +0100

    commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -20,6 +20,10 @@ router.get('/', function (req, res, next) {
     res.render('index', {title: 'Jain Academy Payments', payment_title: "Fundamentals of Jainism : August 2018"});
 });
 
+router.get('/:eventId/tickets', function (req, res, next) {
+    res.render('index', {title: 'Jain Academy Payments', payment_title: "Fundamentals of Jainism : August 2018"});
+});
+
 router.post('/:eventId/tickets/issue', function (req, res, next) {
 
     var ref = shortid.generate();
